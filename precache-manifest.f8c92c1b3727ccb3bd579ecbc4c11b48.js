b435ae86c2eea42abea1f13b6fb9a726c13bb312
Robin Punnoose <robin.punnoose@protonmail.com>
2023-02-15 13:31:05 -0600

    Updates

--- precache-manifest.f8c92c1b3727ccb3bd579ecbc4c11b48.js
+++ precache-manifest.f8c92c1b3727ccb3bd579ecbc4c11b48.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "b316b1e52ebbe22e28bc",
-    "url": "/react-todo/static/css/main.4b6c0689.chunk.css"
+    "revision": "9d41db512002572486ea",
+    "url": "/react-todo/static/css/main.5dd27603.chunk.css"
   },
   {
-    "revision": "b316b1e52ebbe22e28bc",
-    "url": "/react-todo/static/js/main.b316b1e5.chunk.js"
+    "revision": "9d41db512002572486ea",
+    "url": "/react-todo/static/js/main.9d41db51.chunk.js"
   },
   {
     "revision": "dca26ec57be12e129b13",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/react-todo/static/js/runtime~main.5bee33ad.js"
   },
   {
-    "revision": "28c9f3b7ff0ddcdf2dd650649ca0014f",
+    "revision": "8d6f1e6db80cb9164b61de6a287dc37b",
     "url": "/react-todo/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-todo/precache-manifest.0f848fe09d4aac246c0e4b4bea89944f.js"
+  "/react-todo/precache-manifest.f8c92c1b3727ccb3bd579ecbc4c11b48.js"
 );
 
 workbox.clientsClaim();
